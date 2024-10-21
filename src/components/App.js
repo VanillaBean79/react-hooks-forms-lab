b0a1ecef400b031f5ddcbe13d3693015670bf793
Ruben Salazar <rubensalazar@rocketmail.com>
2024-10-21 14:14:14 -0500

    hooks forms lab

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,24 @@ import itemData from "../data/items";
 function App() {
   const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
+  // const stateReturn = useState(0)
+  // console.log(stateReturn)
+  // const counter = stateReturn[0]
 
+  let counter = 0
   function handleDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
+    // setIsDarkMode((isDarkMode) => !isDarkMode);
+    counter +=1
+    console.log('counter', counter)
+
   }
+  // console.log('counter', counter)
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
       <ShoppingList items={items} />
+      <h1>{counter}</h1>
     </div>
   );
 }
